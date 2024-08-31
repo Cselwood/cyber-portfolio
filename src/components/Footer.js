ff2b6f6e97e98da23eab350e044d41537f047a66
CSelwood <cselwood1806@hotmail.co.uk>
2024-08-31 13:50:31 +0100

    background updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,13 @@ function Footer() {
             <p className="small-padding-top white">Deployed in </p> 
             <b><a className="footer-link medium-padding-bottom chartreuse chartreuse-hover" href="https://www.vercel.com" alt="Vercel.com" target="_blank" rel="noreferrer"> Vercel.</a></b>
 
+            <b className="small-padding-top"><a className=" chartreuse chartreuse-hover" href="https://www.svgbackgrounds.com/set/free-svg-backgrounds-and-patterns/">Free SVG Backgrounds and Patterns by SVGBackgrounds.com </a></b>
+
             <h4 className="medium-padding-top small-padding-bottom spaced white">ALL RIGHTS RESERVED</h4>
 
             <h4 className="white">&copy; Chris Selwood</h4>
+
+            
 
         </div>
     );
