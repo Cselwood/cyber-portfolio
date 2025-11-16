1eaa04033faccddd191a826274faf9706d475201
CSelwood <cselwood1806@hotmail.co.uk>
2025-11-16 20:26:39 +0000

    Update to rollback hero text details and accompanying pdf

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,7 +12,9 @@ function Card() {
             
             </h2>
             <h4 className='alt small-margin-bottom small-margin-top'>
-                A Distinction MSc Cyber Security graduate (2025) and ISC2 Certified in Cybersecurity (CC) professional, seeking graduate roles in cyber security. <br/><br/>Focused on AI-driven threat intelligence, network security, ISO-compliant ISMS design, red team operations, vulnerability assessments, and applied cryptography. Currently studying towards Security+ (CompTIA). Known for adapting quickly, simplifying complex problems, and turning technical insight into measurable success.
+                Distinction MSc Cyber Security graduate (2025) and ISC2 Certified in Cybersecurity (CC) with experience in data analysis, software engineering, and security operations across retail and technology. Currently studying towards Security+ (CompTIA).
+                    <br/><br/>
+                Skilled in AI-driven security, ISO/NIST-aligned systems, and threat mitigation. Known for simplifying complex problems and turning technical insight into measurable results.
             </h4>
             <div className='button-flex small-margin-bottom small-padding-bottom'>  
                 <a className='blue-button small-margin-top right-hand-margin' href="https://www.linkedin.com/messaging/compose/?recipient=chris-selwood" alt="linkedin" rel="noreferrer" target='_blank'>
